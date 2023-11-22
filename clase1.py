e1ef460901f4736a9b5e5e9c73e3d5bc33f20df2
Stalin Arturo <villacisestalin12@gmail.com>
2023-11-22 00:52:33 -0500

    on branch

---
+++ clase1.py
@@ -0,0 +1,11 @@
+a="Bienvenido a mi 'Primer' programa"
+b='en el lenguaje de programación "Python"'
+print(a+" "+b)
+b="con el uso de Python"
+print(a,b,'gracias')
+
+a=3
+b=5
+c=7
+
+print(f"El número {a} es el primer valor impar, el segundo es {b} y el tercero es {c}")
