e851fe5026040ef546e69aff87e5da095a2b52e4
Stalin Arturo <villacisestalin12@gmail.com>
2023-11-24 22:32:27 -0500

    on branch

---
+++ Generador de contraseñas.py
@@ -0,0 +1,11 @@
+a="Hola Bienvenido a mi primer proyecto"
+b="'GENERADOR DE CONTRASEÑA SEGURO'"
+print(a+" "+b)
+c= "Menú Principal"
+d="Jugar"
+e="Salir"
+print(c)
+print(d)
+print(e)
+f="¿Deseas generar contraseña aleatoria?"
+print(f)
