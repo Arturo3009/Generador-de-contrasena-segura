edf36573b9b70fde628256306047522917b6cad7
Stalin Arturo <villacisestalin12@gmail.com>
2023-11-25 01:02:28 -0500

    commint

--- Generador de contraseñas.py
+++ Generador de contraseñas.py
@@ -2,10 +2,14 @@ a="Hola Bienvenido a mi primer proyecto"
 b="'GENERADOR DE CONTRASEÑA SEGURO'"
 print(a+" "+b)
 c= "Menú Principal"
-d="Jugar"
-e="Salir"
 print(c)
-print(d)
-print(e)
+d= "Jugar"
+e= "Salir"
+print(d+" o "+e)
+modo=input()
 f="¿Deseas generar contraseña aleatoria?"
 print(f)
+opcion1= "SI "
+opcion2= "NO" 
+print(opcion1+" o "+opcion2)
+decision=input()
